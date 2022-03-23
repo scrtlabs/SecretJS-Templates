f0670c15a97dc9878363a034100869943dbe95ed
Assaf Morami <assaf.morami@gmail.com>
2022-03-23 12:54:41 +0200

    Migrate secret.js for 4 & 4:batch

--- 4_transactions/transfer.js
+++ 4_transactions/transfer.js
@@ -1,46 +1,41 @@
-const {
-    EnigmaUtils, SigningCosmWasmClient, Secp256k1Pen, pubkeyToAddress, encodeSecp256k1Pubkey
-} = require("secretjs");
+const { SecretNetworkClient, Wallet } = require("secretjs");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const main = async () => {
-    
-    const mnemonic = process.env.MNEMONIC;
-    const httpUrl = process.env.SECRET_REST_URL;
-    const signingPen = await Secp256k1Pen.fromMnemonic(mnemonic);
-    const pubkey = encodeSecp256k1Pubkey(signingPen.pubkey);
-    const accAddress = pubkeyToAddress(pubkey, 'secret');
+  // Import wallet from mnemonic phrase
+  const wallet = new Wallet(process.env.MNEMONIC);
 
-    const txEncryptionSeed = EnigmaUtils.GenerateNewSeed();
-    const fees = {
-        send: {
-            amount: [{ amount: "80000", denom: "uscrt" }],
-            gas: "80000",
-        },
+  // Create a connection to Secret Network node
+  // Pass in a wallet that can sign transactions
+  // Docs: https://github.com/scrtlabs/secret.js#secretnetworkclient
+  const secretjs = await SecretNetworkClient.create({
+    grpcWebUrl: process.env.SECRET_GRPC_WEB_URL,
+    wallet: wallet,
+    walletAddress: wallet.address,
+    chainId: process.env.SECRET_CHAIN_ID,
+  });
+
+  const tx = await secretjs.tx.bank.send(
+    {
+      amount: [{ amount: "1", denom: "uscrt" }],
+      fromAddress: wallet.address,
+      toAddress: wallet.address, // Set recipient to sender for testing
+    },
+    {
+      gasLimit: 20_000,
+      gasPriceInFeeDenom: 0.25,
+      memo: "send tokens example",
     }
-    const client = new SigningCosmWasmClient(
-        httpUrl,
-        accAddress,
-        (signBytes) => signingPen.sign(signBytes), 
-        txEncryptionSeed, fees
-    );
-    const rcpt = accAddress; // Set recipient to sender for testing
+  );
 
-    //optional memo
-    const memo = 'sendTokens example';
+  console.log("Transaction: ", tx);
+};
 
-    const sent = await client.sendTokens(rcpt, [{amount: "1234", denom: "uscrt"}], memo)
-    console.log('sent', sent)
-
-    // Query the tx result
-    const query = {id: sent.transactionHash}
-    const tx = await client.searchTx(query)
-    console.log('Transaction: ', tx);
-}
-
-main().then(resp => {
+main()
+  .then((resp) => {
     console.log(resp);
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
---
+++ 4_transactions/transfer_batch.js
@@ -0,0 +1,82 @@
+const { SecretNetworkClient, Wallet } = require("secretjs");
+
+require("dotenv").config();
+
+const main = async () => {
+  // Import wallet from mnemonic phrase
+  const wallet = new Wallet(process.env.MNEMONIC);
+
+  // Create a connection to Secret Network node
+  // Pass in a wallet that can sign transactions
+  // Docs: https://github.com/scrtlabs/secret.js#secretnetworkclient
+  const secretjs = await SecretNetworkClient.create({
+    grpcWebUrl: process.env.SECRET_GRPC_WEB_URL,
+    wallet: wallet,
+    walletAddress: wallet.address,
+    chainId: process.env.SECRET_CHAIN_ID,
+  });
+
+  const account = await secretjs.query.auth.account({
+    address: wallet.address,
+  });
+
+  const txHashes = [];
+  for (let i = 0; i < 10; i++) {
+    const tx = await secretjs.tx.bank.send(
+      {
+        amount: [{ amount: "1", denom: "uscrt" }],
+        fromAddress: wallet.address,
+        toAddress: wallet.address, // Set recipient to sender for testing
+      },
+      {
+        gasLimit: 20_000,
+        gasPriceInFeeDenom: 0.25,
+        memo: `send batch tokens example #${i + 1}`,
+        waitForCommit: false,
+        explicitSignerData: {
+          accountNumber: Number(account.account.accountNumber),
+          sequence: Number(account.account.sequence) + i,
+          chainId: process.env.SECRET_CHAIN_ID,
+        },
+      }
+    );
+    txHashes.push(tx.transactionHash);
+  }
+
+  let i = 1;
+  for (const txhash of txHashes) {
+    console.log(`Waiting for tx ${i} (txhash ${txhash})`);
+    let tx;
+    while (true) {
+      tx = await secretjs.query.getTx(txhash);
+      if (tx) {
+        break;
+      }
+      await sleep(100);
+    }
+
+    if (tx.code === 0) {
+      console.log(
+        `tx ${i} (txhash ${txhash}) was successfull in block ${tx.height}`
+      );
+    } else {
+      console.log(
+        `tx ${i} (txhash ${txhash}) was unsuccessfull in block ${tx.height}, error log: ${tx.rawLog}`
+      );
+    }
+
+    i++;
+  }
+};
+
+main()
+  .then((resp) => {
+    console.log(resp);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function sleep(ms) {
+  return new Promise((accept) => setTimeout(accept, ms));
+}
