80736a3ff49392e12aeeda4e9a773ffad0b05342
Taariq Levack <taariql@gmail.com>
2022-08-13 10:20:58 +0200

    Vanilla JS keplr

--- 6_wallets/keplr/webpack.config.js
+++ 6_wallets/keplr/webpack.config.js
@@ -1,21 +1,27 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: {
-        main: "./src/main.js",
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname,  "dist")
-    },
-    devServer: {
+  mode: 'development',
+  entry: {
+    main: "./src/main.js",
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname,  "dist"),
+  },
+  devServer: {
         port: 8081
-    },
-    plugins: [
+  },
+  plugins: [
         new HtmlWebpackPlugin({
             template: "index.html",
             chunks: ["main"]
         })
-    ]
+    ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 5120000,
+    maxAssetSize: 5120000
+  }
 };
